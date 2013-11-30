a15db7a76c6170170505b1e3e3b9591a6465cf36
mohsen alizadeh <0x21hate@gmail.com>
2013-11-30 20:04:10 +0330

    init PDate via Date and implement to_s of PDate

--- lib/rails_pdate/p_date.rb
+++ lib/rails_pdate/p_date.rb
@@ -1,6 +1,15 @@
 # :title:PDate (Persian Date) #
 class PDate
+
   def initialize *args
 
+    if (args.size == 1) && (args.first.is_a?(Date))
+      @year, @month, @day = Pconvertor.new.gregorian_to_persian(args.first.year, args.first.month, args.first.day)
+    end
   end
+
+  def to_s
+    "#{@year}-#{@month}-#{@day}"
+  end
+
 end
--- test/rails_pdate_test.rb
+++ test/rails_pdate_test.rb
@@ -4,4 +4,21 @@ class RailsPdateTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, RailsPdate
   end
+
+  test "sample create persian date object from date 1" do
+    assert_equal PDate.new(Date.new(1990, 1, 29)).to_s, "1368-11-9"
+  end
+
+  test "sample create persian date object from date 2" do
+    assert_equal PDate.new(Date.new(1988, 11, 20)).to_s, "1367-8-29"
+  end
+
+  test "sample create persian date object from date 3" do
+    assert_equal PDate.new(Date.new(2012, 11, 18)).to_s, "1391-8-28"
+  end
+
+  test "sample create persian date object from date 4" do
+    assert_equal PDate.new(Date.new(2013, 4, 19)).to_s, "1392-1-30"
+  end
+
 end
