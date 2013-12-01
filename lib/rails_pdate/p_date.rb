b27e93f4489f744d55e24edf6b8c667041916f18
mohsen alizadeh <0x21hate@gmail.com>
2013-12-01 21:29:06 +0330

    Implement strftime

--- lib/rails_pdate/p_date.rb
+++ lib/rails_pdate/p_date.rb
@@ -1,15 +1,89 @@
+require 'date'
+
 # :title:PDate (Persian Date) #
 class PDate
+
+  DATE_FORMATS = {
+      :short        => '%e %b',
+      :long         => '%B %e, %Y',
+      :db           => '%Y-%m-%d',
+      :number       => '%Y%m%d',
+      :long_ordinal => lambda { |date|
+        day_format = ActiveSupport::Inflector.ordinalize(date.day)
+        date.strftime("%B #{day_format}, %Y") # => "April 25th, 2007"
+      },
+      :rfc822       => '%e %b %Y',
+      :iso8601      => lambda { |date| date.iso8601 },
+      :default      => '%Y-%m-%d'
+  }
 
   def initialize *args
 
     if (args.size == 1) && (args.first.is_a?(Date))
-      @year, @month, @day = Pconvertor.new.gregorian_to_persian(args.first.year, args.first.month, args.first.day)
+      year, month, day = Pconvertor.new.gregorian_to_persian(args.first.year, args.first.month, args.first.day)
+    else
+      year,month,day,hour,min,sec,zone,utc_offset = args
+    end
+
+    @year = year
+    @month = month
+    @day = day
+    @hour ||= hour || 0
+    @min ||= min || 0
+    @sec ||= sec || 0
+    @zone ||= zone || "UTC"
+    @utc_offset ||= utc_offset || 0
+  end
+
+  def to_date
+    year, month, day = Pconvertor.new.persian_to_gregorian(@year, @month, @day)
+    Date.new(year, month, day)
+  end
+
+
+  def to_formatted_s(format = :default)
+    if formatter = DATE_FORMATS[format]
+      if formatter.respond_to?(:call)
+        formatter.call(self).to_s
+      else
+        strftime(formatter)
+      end
+    else
+      to_default_s
     end
   end
+  alias_method :to_default_s, :to_s
+  alias_method :to_s, :to_formatted_s
 
-  def to_s
-    "#{@year}-#{@month}-#{@day}"
+  # Overrides the default inspect method with a human readable one, e.g., "Mon, 21 Feb 2005"
+  def readable_inspect
+    strftime('%a, %d %b %Y')
+  end
+  alias_method :default_inspect, :inspect
+  alias_method :inspect, :readable_inspect
+
+  # Converts a Date instance to a Time, where the time is set to the beginning of the day.
+  # The timezone can be either :local or :utc (default :local).
+  #
+  #   date = Date.new(2007, 11, 10)  # => Sat, 10 Nov 2007
+  #
+  #   date.to_time                   # => Sat Nov 10 00:00:00 0800 2007
+  #   date.to_time(:local)           # => Sat Nov 10 00:00:00 0800 2007
+  #
+  #   date.to_time(:utc)             # => Sat Nov 10 00:00:00 UTC 2007
+  def to_time(form = :local)
+    ::Time.send(form, year, month, day)
   end
 
+  def xmlschema
+    in_time_zone.xmlschema
+  end
+
+  def strftime(format)
+    format
+      .gsub('%Y', '%d' % @year)
+      .gsub('%m', '%02d' % @month)
+      .gsub('%d', '%02d' % @day)
+      #.gsub('%C', '%d' % (@year / 100) )
+  end
 end
--- test/rails_pdate_test.rb
+++ test/rails_pdate_test.rb
@@ -1,24 +1,43 @@
 require 'test_helper'
 
 class RailsPdateTest < ActiveSupport::TestCase
+
   test "truth" do
     assert_kind_of Module, RailsPdate
   end
 
+  # creating PDate from Date
   test "sample create persian date object from date 1" do
-    assert_equal PDate.new(Date.new(1990, 1, 29)).to_s, "1368-11-9"
+    assert_equal PDate.new(Date.new(1990, 1, 29)).to_s, "1368-11-09"
   end
 
   test "sample create persian date object from date 2" do
-    assert_equal PDate.new(Date.new(1988, 11, 20)).to_s, "1367-8-29"
+    assert_equal PDate.new(Date.new(1988, 11, 20)).to_s, "1367-08-29"
   end
 
   test "sample create persian date object from date 3" do
-    assert_equal PDate.new(Date.new(2012, 11, 18)).to_s, "1391-8-28"
+    assert_equal PDate.new(Date.new(2012, 11, 18)).to_s, "1391-08-28"
   end
 
   test "sample create persian date object from date 4" do
-    assert_equal PDate.new(Date.new(2013, 4, 19)).to_s, "1392-1-30"
+    assert_equal PDate.new(Date.new(2013, 4, 19)).to_s, "1392-01-30"
+  end
+
+  # creating Date from PDate
+  test "sample create date object from persian date 1" do
+    assert_equal PDate.new(1368, 11, 9).to_date.to_s, "1990-01-29"
+  end
+
+  test "sample create date object from persian date 2" do
+    assert_equal PDate.new(1367, 8, 29).to_date.to_s, "1988-11-20"
+  end
+
+  test "sample create date object from persian date 3" do
+    assert_equal PDate.new(1391, 8, 28).to_date.to_s, "2012-11-18"
+  end
+
+  test "sample create date object from persian date 4" do
+    assert_equal PDate.new(1392, 1, 30).to_date.to_s, "2013-04-19"
   end
 
 end
