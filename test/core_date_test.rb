ee827d7e987181193d402ef5a06cce9edbe2bec7
mohsen alizadeh <0x21hate@gmail.com>
2013-12-05 17:58:16 +0330

    Add some UnitTests to to_pdate method of Date

--- test/core_date_test.rb
+++ test/core_date_test.rb
@@ -7,6 +7,11 @@ class CoreDateTest < ActiveSupport::TestCase
     assert_equal Date.today.to_pdate.class, PDate
   end
 
+  test "date to pdate 1" do
+    assert_equal Date.new(1990, 1, 29).to_pdate.to_a, [1368, 11, 9]
+  end
 
-
+  test "date to pdate 2" do
+    assert_equal Date.new(1988, 11, 20).to_pdate.to_a, [1367, 8, 29]
+  end
 end
