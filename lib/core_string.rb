bbb3ab7c87cdbbba3c67244245e8abf917efd24c
mohsen alizadeh <0x21hate@gmail.com>
2013-12-06 18:49:35 +0330

    Add to_pdate to String class to parse string to PDate

--- lib/core_string.rb
+++ lib/core_string.rb
@@ -1,5 +1,7 @@
 String.class_eval do
   def to_pdate
 
+    date = Date._parse(self)
+    return PDate.new(date[:year], date[:mon], date[:mday])
   end
 end
--- test/core_string_test.rb
+++ test/core_string_test.rb
@@ -3,4 +3,15 @@ require 'test_helper'
 
 class CoreDateTest < ActiveSupport::TestCase
 
+  test "parse string to pdate with format: %Y-%m-%d 1" do
+    assert_equal "1368-11-09".to_pdate.to_a, [1368, 11, 9]
+  end
+
+  test "parse string to pdate with format: %Y-%m-%d 3" do
+    assert_equal "1368/11/09".to_pdate.to_a, [1368, 11, 9]
+  end
+
+  test "parse string to pdate with format: %Y-%m-%d 2" do
+    assert_equal "1367-08-29".to_pdate.to_a, [1367, 8, 29]
+  end
 end
