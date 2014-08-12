cd10309bb7d145b73fdb7ed4fd8a63296f3a649f
mohsen alizadeh <0x21hate@gmail.com>
2014-08-12 13:44:37 +0430

    bug fix in parsing 2 digits year in date string

--- lib/core_string.rb
+++ lib/core_string.rb
@@ -1,7 +1,17 @@
 String.class_eval do
   def to_pdate
 
-    date = Date._parse(self)
-    return PDate.new(date[:year], date[:mon], date[:mday])
+    if result = self.match(/(\d{2,4})(\/|-)(\d{1,2})(\/|-)(\d{1,2})/)
+
+      year, sp, week, sp, day = result.captures
+
+      if year.to_i < 100
+        year = 1300 + year.to_i
+      end
+
+      return PDate.new(year.to_i, week.to_i, day.to_i)
+    else
+      return nil
+    end
   end
 end
